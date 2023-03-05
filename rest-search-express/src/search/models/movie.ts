6b7b7565c60fbd57cf9e6014520a6c702849edeb
Ovais Tariq <ovaistariq@gmail.com>
2023-03-04 20:45:22 -0800

    Update the rest-search-express template (#5)

--- rest-search-express/src/search/models/movie.ts
+++ rest-search-express/src/search/models/movie.ts
@@ -1,18 +1,18 @@
-import { IndexField, TigrisDataTypes, TigrisIndex } from "@tigrisdata/core";
+import { SearchField, TigrisDataTypes, TigrisSearchIndex } from "@tigrisdata/core";
 
 export const MOVIE_INDEX_NAME = "movie";
 
-@TigrisIndex(MOVIE_INDEX_NAME)
+@TigrisSearchIndex(MOVIE_INDEX_NAME)
 export class Movie {
-  @IndexField({ sort: true })
+  @SearchField({ sort: true })
   title: string;
 
-  @IndexField({ sort: true, facet: true })
+  @SearchField({ sort: true, facet: true })
   year: number;
 
-  @IndexField({ elements: TigrisDataTypes.STRING, facet: true })
+  @SearchField({ elements: TigrisDataTypes.STRING, facet: true })
   cast: string[];
 
-  @IndexField({ elements: TigrisDataTypes.STRING, facet: true })
+  @SearchField({ elements: TigrisDataTypes.STRING, facet: true })
   genres: string[];
 }
